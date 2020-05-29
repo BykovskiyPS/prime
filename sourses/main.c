61b500e0808ada853515457c74df644013866869
pavel <bikovsky.pavel@yandex.ru>
2020-05-29 12:07:10 +0300

    add sourses

---
+++ sourses/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern void PrimeNumbers(int count);
+
+int main(int argc, char* argv[]) {
+    if(argc != 2) {
+        printf("Error: enter one number");
+        exit(1);
+    }
+    PrimeNumbers(atoi(argv[1]));
+    return 0;
+}
