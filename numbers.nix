daad74656c9f6148b557acf294ca2d0bcc5d941c
pavel <bikovsky.pavel@yandex.ru>
2020-05-29 12:29:40 +0300

    change nix file

--- numbers.nix
+++ numbers.nix
@@ -4,7 +4,7 @@ stdenv.mkDerivation {
   name = "numbers-0.1";
   builder = ./builder.sh;
   src fetchurl {
-    url =  ;
-    sha256 = "";
+    url =  https://github.com/BykovskiyPS/prime.git;
+    sha256 = ""07q5bl5ld3yx19n08qsibbagkbqibsk6znqa4b5lb3fcwgc88f05;
   };
 } 
