42c0fd41848229ed35db3570561621f693edb891
pavel <bikovsky.pavel@yandex.ru>
2020-06-01 22:15:03 +0300

    chage numbers.nix

--- numbers.nix
+++ numbers.nix
@@ -4,8 +4,8 @@ let
 
   nixpkgs = import <nixpkgs> {};
 
-  prime_lib_commit = "eb4aed1377704bf69771042ef89d0b07271a7a26";
-  prime_lib = pkgs.callPackage(buiktins.fetchTarball{
+  lib_prime_commit = "eb4aed1377704bf69771042ef89d0b07271a7a26";
+  libpirme = pkgs.callPackage(buiktins.fetchTarball{
     url = "https://github.com/BykovskiyPS/prime_lib/archive/prime_lib.tar.gz";
     sha256 = "19153jq0ix097g9w7iw5x4q6mv7kcqa4naqp6jnbpv9d9dcv3b30";
   }) {};
