157acc3be4fc05c0139f343a538d5f7e7227e7d5
pavel <bikovsky.pavel@yandex.ru>
2020-06-01 22:04:56 +0300

    chage numbers.nix

--- numbers.nix
+++ numbers.nix
@@ -4,17 +4,18 @@ let
 
   nixpkgs = import <nixpkgs> {};
 
-  inherit (nixpkgs) stdenv fetchurl which;
-
-  libprime = import ./default.nix;
-
-  numbers = stdenv.mkDerivation {
+  prime_lib_commit = "eb4aed1377704bf69771042ef89d0b07271a7a26";
+  prime_lib = pkgs.callPackage(buiktins.fetchTarball{
+    url = "https://github.com/BykovskiyPS/prime_lib/archive/prime_lib.tar.gz";
+    sha256 = "19153jq0ix097g9w7iw5x4q6mv7kcqa4naqp6jnbpv9d9dcv3b30";
+  }) {};
+in 
+stdenv.mkDerivation {
     name = "numbers-0.1";
  
     src  =  ./.;
     buildInputs = [ libprime ];
-    inherit libprime;
-
+    
     buildPhase = ''
       gcc -o numbers main.c $libprime/lib/libprime.so
     '';
@@ -23,6 +24,4 @@ let
       mkdir -p $out/bin
       cp numbers $out/bin
     '';
-};
-
-in numbers
+}
